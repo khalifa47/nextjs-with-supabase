adfa600e4f8303e2032927d479345412274c0b63
khalifa47 <khalifafumo5@gmail.com>
2024-04-27 19:19:10 +0300

    chore: update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Just-Do-It",
+  description: "The one-stop to-do app for all your tasks",
 };
 
 export default function RootLayout({
